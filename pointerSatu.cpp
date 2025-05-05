99ea64fa5801ae9bcf85e6a179a84dbe617bcc3f
BimoFr <bimofadhlurrahman@gmail.com>
2025-05-05 16:05:26 +0700

    Menambahkan objek mahasiswa dan showNim, dan memodifikasi nim menggunakan reference dan pointer

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -8,3 +8,19 @@ class Mahasiswa{
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main (){
+    
+    Mahasiswa mhs;
+    mhs.nim = 5;
+    mhs.showNim();
+
+    Mahasiswa &refMhs = mhs;
+    refMhs.nim = 2;
+    mhs.showNim();
+
+    Mahasiswa *pMhs = &mhs;
+    pMhs->nim = 3;
+    pMhs->showNim();
+    return 0;
+}
